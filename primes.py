92dcef5edecfefc78a17cb4f5f0e71f83ca4c214
Stephanie Resis <sresis@umich.edu>
2020-09-29 15:37:55 -0500

    completed problem

--- primes.py
+++ primes.py
@@ -16,6 +16,26 @@ For example::
 
 def primes(count):
     """Return count number of prime numbers, starting at 2."""
+    primes_list = []
+    if count == 0:
+        return []
+    
+    i = 2
+    while len(primes_list) < count:
+ 
+        if i == 2:
+            primes_list.append(2)
+        # see if it is divisible by any of the primes so far
+        else:
+            divis_count = 0
+            for num in primes_list:
+                if i % num == 0:
+                    divis_count += 1
+            if divis_count == 0:
+                primes_list.append(i)
+        i += 1
+    return primes_list
+
 
 
 if __name__ == '__main__':
